9a8218f0ef05c9dfc6375f28709873c035e6aeb4
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-14 16:18:51 -0800

    Create generation engine

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,74 @@
-const Dragon = require("./dragon");
+const  GenerationEngine = require('./engine');
 
-const Alpha = new Dragon({
-  birthday: new Date(), 
-  nickname: "A"
-});
+const engine = new GenerationEngine();
 
-const Beta = new Dragon({
-  birthday: new Date(), 
-  nickname: "B",
-  traits: [
-    {traitType: 'backgroundColor', traitValue: "green"}
-  ]
-});
-
-const Gamma = new Dragon();
+engine.start();
 
 setTimeout(() => {
-  const Delta = new Dragon();
-  console.log('Delta', Delta);
-}, 3000);
+  engine.stop();
+}, 20000);
 
-console.log('Alpha', Alpha);
-console.log('Bata', Beta);
-console.log('Gamma', Gamma);
+
+// const Generation = require('./generation.js');
+
+// const generation = new Generation();
+
+// console.log('generation', generation);
+
+// const alpha = generation.newDragon();
+
+// console.log("alpha", alpha);
+
+// setTimeout(() => {
+//   const beta = generation.newDragon();
+//   console.log("beta", beta);
+// }, 15000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Dragon = require("./dragon");
+
+// const Alpha = new Dragon({
+//   birthday: new Date(), 
+//   nickname: "A"
+// });
+
+// const Beta = new Dragon({
+//   birthday: new Date(), 
+//   nickname: "B",
+//   traits: [
+//     {traitType: 'backgroundColor', traitValue: "green"}
+//   ]
+// });
+
+// const Gamma = new Dragon();
+
+// setTimeout(() => {
+//   const Delta = new Dragon();
+//   console.log('Delta', Delta);
+// }, 3000);
+
+// console.log('Alpha', Alpha);
+// console.log('Bata', Beta);
+// console.log('Gamma', Gamma);
 
 
