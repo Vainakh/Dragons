ec9ed1e6f02384aa1b13380a0228a384d5f05351
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-14 19:15:52 -0800

    Make change to codebase organization

---
+++ backend/app/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const  GenerationEngine = require('./generation/engine');
+
+const app = express();
+const engine = new GenerationEngine();
+
+
+engine.start();
+
+// setTimeout(() => {
+//   engine.stop();
+// }, 20000);
+
+app.get("/dragon/new", (req, res) => {
+  res.json({dragon: engine.generation.newDragon()});
+});
+
+module.exports = app;
+
+
+// const Generation = require('./generation.js');
+
+// const generation = new Generation();
+
+// console.log('generation', generation);
+
+// const alpha = generation.newDragon();
+
+// console.log("alpha", alpha);
+
+// setTimeout(() => {
+//   const beta = generation.newDragon();
+//   console.log("beta", beta);
+// }, 15000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Dragon = require("./dragon");
+
+// const Alpha = new Dragon({
+//   birthday: new Date(), 
+//   nickname: "A"
+// });
+
+// const Beta = new Dragon({
+//   birthday: new Date(), 
+//   nickname: "B",
+//   traits: [
+//     {traitType: 'backgroundColor', traitValue: "green"}
+//   ]
+// });
+
+// const Gamma = new Dragon();
+
+// setTimeout(() => {
+//   const Delta = new Dragon();
+//   console.log('Delta', Delta);
+// }, 3000);
+
+// console.log('Alpha', Alpha);
+// console.log('Bata', Beta);
+// console.log('Gamma', Gamma);
+
+
