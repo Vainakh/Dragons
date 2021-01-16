e8b73dd213a53011441abe8f6c82a84f7fa0411c
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-16 10:08:11 -0800

    Store dragons into the database

---
+++ backend/app/dragon/table.js
@@ -0,0 +1,23 @@
+const pool = require('../../databasePool');
+
+class DragonTable {
+  static storeDragon(dragon) {
+    const { birthday, nickname, generationId} = dragon;
+
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'INSERT INTO dragon(birthday, nickname, "generationId") VALUES($1, $2, $3) RETURNING id',
+        [birthday, nickname, generationId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          const dragonId = response.rows[0].id;
+
+          resolve({dragonId });
+        }
+      )
+    });
+  }
+}
+
+module.exports = DragonTable;
