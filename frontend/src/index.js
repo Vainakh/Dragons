b4604f5cdce1edc5757e766cbef82f34d15dc6b1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-17 12:07:56 -0800

    Create generation component

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Generation from './components/Generations';
+
+render(
+  <div>
+    <h2>Dragon Stack from React</h2>
+    <Generation/>
+  </div>,
+  document.getElementById("root")
+);
+
