face8979050779fcc32e5d90c4d032d4bdd50fd1
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-26 11:39:58 -0800

    Get account dragons request

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const AccountTable = require('../account/table.js');
+const AccountDragonTable = require('../accountDragon/table');
 const Session = require('../account/session');
 const { hash } = require('../account/helper');
 const { setSession, authenticatedAccount } = require('./helper');
@@ -63,12 +64,19 @@ router.post('/signup', (req, res, next) => {
   });
 
   router.get('/authenticated', (req, res, next) => {
-    const { sessionString } = req.cookies;
-
-    authenticatedAccount({ sessionString })
+    authenticatedAccount({ sessionString: req.cookies.sessionString })
     .then(({ authenticated }) => res.json({ authenticated }))
     .catch(error => next(error)); 
   });
-  
+
+  router.get('/dragons', (req, res, next) => {
+    authenticatedAccount({ sessionString: req.cookies.sessionString })
+    .then(({ account }) => {
+      return AccountDragonTable.getAccountDragons({ accountId: account.id 
+      })
+    }) 
+    .then(({ accountDragons }) =>  res.json({  accountDragons }))
+    .catch(error => next(error)); 
+  }); 
 
 module.exports = router;
