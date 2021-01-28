d2294a02e92829996eccb0ffa57ff384c2750d1c
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-27 18:42:37 -0800

    Update dragon nickname

--- frontend/dist/src.e31bb0bc.js
+++ frontend/dist/src.e31bb0bc.js
@@ -54463,7 +54463,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64675" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51569" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
