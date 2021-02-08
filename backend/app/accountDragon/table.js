e261db4236e6380475755dfbee947521bf8e9734
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-08 10:38:15 -0800

    Get dragon account and update dragon account

--- backend/app/accountDragon/table.js
+++ backend/app/accountDragon/table.js
@@ -1,4 +1,5 @@
 const pool = require('../../databasePool');
+const AccountTable = require('../account/table');
 
 class  AccountDragonTable {
   static storeAccountDragon({ accountId, dragonId }) {
@@ -27,7 +28,42 @@ class  AccountDragonTable {
         }
       )
     })
+  }
+  
+  static getDragonAccount({ dragonId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'SELECT "accountId" FROM accountDragon WHERE "dragonId" = $1',
+        [dragonId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          resolve({ accountId: response.rows[0].accountId })
+        }
+      )
+    })
+  }
+
+  static updateDragonAccount({ dragonId, accountId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'UPDATE accountDragon SET "accountId" = $1 WHERE "dragonId" = $2',
+      [accountId, dragonId],
+      (error, response) => {
+        if (error) return reject(error);
+
+        resolve();
+      })
+    })
   } 
 }
+
+AccountDragonTable.getDragonAccount({ dragonId: 3})
+.then(({ accountId}) => console.log('accountId', accountId ))
+.catch(error => console.error('error', error));
+
+AccountDragonTable.updateDragonAccount({ dragonId: 2, accountId: 99 })
+.then(() => console.log('success'))
+.catch(error => console.log('error', error));
 
 module.exports = AccountDragonTable;
